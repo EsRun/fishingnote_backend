b105dfeb0f9062b4d920497c6a9748fc9d84434a
비엔나 <sure@paran.com>
2022-03-26 13:20:01 +0900

    init

---
+++ src/main/java/com/fishing/www/FishingnoteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fishing.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishingnoteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishingnoteBackendApplication.class, args);
+	}
+
+}
